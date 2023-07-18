0a1b88dce9199df47a4c1c86896b890a6837cf9c
Léon van der Kaap <leonkaap@gmail.com>
2023-07-18 17:13:04 +0200

    Changed project compat to Zig 0.11.

--- build.zig
+++ build.zig
@@ -1,21 +1,33 @@
 const std = @import("std");
 const version = @import("version.zig");
 const builtin = std.builtin;
+pub const Step = std.Build.Step;
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build.Builder) void {
+    const lib = b.addStaticLibrary(.{
+    .name = "hashes",
+    .root_source_file = .{.path = "src/std.zig"},
+    .optimize = .ReleaseSafe,
+    .version = .{.major = version.major, .minor = version.minor, .patch = version.patch},
+    .target = .{}
+    });
+    b.installArtifact(lib);
 
-    b.setPreferredReleaseMode(builtin.Mode.ReleaseSafe);
-    const mode = b.standardReleaseOptions();
-    
-
-    const lib = b.addStaticLibrary("hashes", "src/std.zig");
-    _ = b.version(version.major, version.minor, version.patch);
-    lib.setBuildMode(mode);
-    lib.install();
-
-    var main_tests = b.addTest("src/jenkins.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = addTests(b, &.{
+    .{.root_source_file = .{.path = "src/std.zig"}},
+    .{.root_source_file = .{.path = "src/djb2.zig"}},
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+}
+
+
+fn addTests(b: *std.Build.Builder, tests: []const std.Build.TestOptions) *Step.Compile {
+    var result: *Step.Compile = undefined;
+    for (tests) |testElem| {
+        result = b.addTest(testElem);
+    }
+    return result;
 }
