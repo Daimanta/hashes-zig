91cbc7d43573ac5b7cd496aa90d5130cec6fdd6c
Léon van der Kaap <leonkaap@gmail.com>
2025-03-07 18:44:44 +0100

    Set Zig compatibility to 0.14

--- build.zig
+++ build.zig
@@ -1,30 +1,41 @@
 const std = @import("std");
 const version = @import("version.zig");
 const builtin = std.builtin;
-pub const Step = std.Build.Step;
 
-pub fn build(b: *std.Build.Builder) void {
+const Builder = std.Build;
+const Step = std.Build.Step;
+
+
+pub fn build(b: *Builder) void {
+    const current_zig_version = @import("builtin").zig_version;
+    if (current_zig_version.major != 0 or current_zig_version.minor < 14) {
+        std.debug.print("This project does not compile with a Zig version <0.14.x. Exiting.", .{});
+        std.os.exit(1);
+    }
+
+    const target = b.standardTargetOptions(.{});
+
     const lib = b.addStaticLibrary(.{
-    .name = "hashes",
-    .root_source_file = .{.path = "src/std.zig"},
-    .optimize = .ReleaseSafe,
-    .version = .{.major = version.major, .minor = version.minor, .patch = version.patch},
-    .target = .{}
+        .name = "hashes",
+        .root_source_file = b.path("src/std.zig"),
+        .optimize = .ReleaseSafe,
+        .version = .{.major = version.major, .minor = version.minor, .patch = version.patch},
+        .target = target
     });
     b.installArtifact(lib);
 
+
     const main_tests = addTests(b, &.{
-    .{.root_source_file = .{.path = "src/std.zig"}},
-    .{.root_source_file = .{.path = "src/djb2.zig"}},
+    .{.root_source_file = b.path("src/std.zig")},
+    .{.root_source_file = b.path("src/djb2.zig")},
     });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
-
 }
 
 
-fn addTests(b: *std.Build.Builder, tests: []const std.Build.TestOptions) *Step.Compile {
+fn addTests(b: *Builder, tests: []const std.Build.TestOptions) *Step.Compile {
     var result: *Step.Compile = undefined;
     for (tests) |testElem| {
         result = b.addTest(testElem);
